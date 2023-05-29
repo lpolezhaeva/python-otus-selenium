8af3545341702673a81a7d96d3ae613685964cec
lpolezhaeva <kroshic555@mail.ru>
2023-05-29 18:27:44 +0200

    Run Opencart in Docker

--- conftest.py
+++ conftest.py
@@ -1,3 +1,5 @@
+import logging
+
 import pytest
 import os
 from selenium import webdriver
@@ -5,17 +7,38 @@ from selenium.webdriver.chrome.service import Service as ChromiumService
 from selenium.webdriver.firefox.service import Service as FFService
 from selenium.webdriver.edge.service import Service as EdgeService
 
+from selenium.webdriver.chrome.options import Options
+
+logging.basicConfig(level=logging.INFO)
+mylogger = logging.getLogger()
+
 
 def pytest_addoption(parser):
     parser.addoption(
         "--browser", default="chrome", help="Browser to run tests"
     )
+    # parser.addoption(
+    #     "--drivers", default=os.path.expanduser("~/Downloads/drivers"), help="Drivers path"
+    # )
     parser.addoption(
-        "--drivers", default=os.path.expanduser("~/Downloads/drivers"), help="Drivers path"
+        "--drivers", default="/chromedriver", help="Drivers path"
     )
     parser.addoption(
         "--url", default="http://127.0.0.1:8081", help="Opencart base URL"
     )
+
+def set_chrome_options() -> Options:
+    """Sets chrome options for Selenium.
+    Chrome options for headless browser is enabled.
+    """
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_prefs = {}
+    chrome_options.experimental_options["prefs"] = chrome_prefs
+    chrome_prefs["profile.default_content_settings"] = {"images": 2}
+    return chrome_options
 
 
 @pytest.fixture(scope="session")
@@ -31,11 +54,12 @@ def browser(request):
 
     driver = None
 
+    # it works now only for chrome
     if browser == "chrome":
-        service = ChromiumService(executable_path=drivers + "/chromedriver")
-        driver = webdriver.Chrome(service=service)
+        service = ChromiumService()
+        driver = webdriver.Chrome(service=service, options=set_chrome_options())
     elif browser == "firefox" or browser == "ff":
-        service = FFService(executable_path=drivers + "/geckodriver")
+        service = FFService(executable_path="/geckodriver")
         driver = webdriver.Firefox(service=service)
     elif browser == "edge":
         service = EdgeService(executable_path=drivers)
